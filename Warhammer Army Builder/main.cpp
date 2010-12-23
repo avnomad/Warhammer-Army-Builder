539b24bfbf4bbff82446fca7aa5279bb1f985845
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2010-12-23 16:56:22 +0200

    now creates a listbox and fills it from file.

--- Warhammer Army Builder/main.cpp
+++ Warhammer Army Builder/main.cpp
@@ -5,6 +5,17 @@ using std::endl;
 using std::cerr;
 using std::clog;
 using std::left;
+using std::right;
+
+#include <iomanip>
+using std::setw;
+using std::setprecision;
+
+#include <fstream>
+using std::ifstream;
+using std::ofstream;
+using std::fstream;
+using std::ios;
 
 #include <cstdlib>
 using std::system;
@@ -15,8 +26,13 @@ using std::ostringstream;
 using std::istringstream;
 using std::stringstream;
 
+#include <string>
+using std::string;
+using std::getline;
+
 #include <windows.h>
 
+#define length(A) (sizeof(A)/sizeof((A)[0]))
 
 
 HRESULT CALLBACK armyBuilderProc(HWND window, UINT message, WPARAM argW, LPARAM argL);
@@ -56,13 +72,39 @@ int main()
 	return message.wParam;
 } // end function main
 
+enum ChildIDs {UNIT_LIST_ID};
 
 HRESULT CALLBACK armyBuilderProc(HWND window, UINT message, WPARAM argW, LPARAM argL)
 {
+	static INT tab_stops[] = {120};
+	HWND unitList;
+	ifstream in;
+	string name;
+	unsigned int points;
+	ostringstream entry;
+
 	switch(message)
 	{
 	case WM_CREATE:
 		SetClassLong(window,0,GetClassLong(window,0) + 1);	// increase the window count by 1
+
+		unitList = CreateWindow(TEXT("listbox"),TEXT("TEST"),WS_CHILD|WS_VISIBLE|LBS_NOTIFY|LBS_SORT|WS_BORDER|WS_VSCROLL|LBS_USETABSTOPS,
+						60,40,290,400,window,(HMENU)UNIT_LIST_ID,(HINSTANCE)GetWindowLong(window,GWL_HINSTANCE),NULL);
+
+		in.exceptions(std::ios_base::failbit);
+		in.open(TEXT("units.txt"));
+
+		SendMessage(unitList,LB_SETTABSTOPS,(WPARAM)length(tab_stops),(LPARAM)tab_stops);
+		while(in)
+		{
+			getline(in,name,'\t') >> points >> std::ws;	// skip newline
+			entry << name << '\t' << points;
+			SendMessage(unitList,LB_ADDSTRING,0,(LPARAM)entry.str().c_str());
+			entry.str("");
+		} // end while
+
+		in.close();
+
 		return 0;
 	case WM_DESTROY:
 		if(SetClassLong(window,0,GetClassLong(window,0) - 1) <= 1)	// decrease the window count by 1
