cbc1e49c9256d7803479a6534c3c4e0da347745b
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-05-25 04:19:04 +0300

    Added copyright notices and license.
    Had to make a small change to the code loading units.txt to skip comments.

--- Warhammer Army Builder/main.cpp
+++ Warhammer Army Builder/main.cpp
@@ -1,3 +1,21 @@
+//	Copyright (C) 2010-2012 Vaptistis Anogeianakis <el05208@mail.ntua.gr>
+/*
+ *	This file is part of Warhammer Army Builder.
+ *
+ *	Warhammer Army Builder is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Warhammer Army Builder is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Warhammer Army Builder.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -127,7 +145,8 @@ LRESULT CALLBACK armyBuilderProc(HWND window, UINT message, WPARAM argW, LPARAM 
 		while(in)
 		{
 			getline(in,name);
-			SendMessage(codex_listbox,LB_ADDSTRING,0,(LPARAM)name.c_str());
+			if(2 < name.size() && name[0] != '/' && name[1] != '*')	// skip comment header
+				SendMessage(codex_listbox,LB_ADDSTRING,0,(LPARAM)name.c_str());
 		} // end while
 		in.close();
 		SendMessage(codex_listbox,LB_SETCURSEL,0,0);	// why doesn't work?
